e6a5a12e635a076610a1ca1ed472d430b1dd101a
Oscar Monroy <oscarmonroy657@gmail.com>
2021-10-22 19:21:20 -0600

    Fade in ready.

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,6 +5,7 @@ import { ActivityIndicator, Button, FlatList, Image, Text, View } from 'react-na
 import { globalTheme } from '../theme/appTheme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { usePokemonPagineted } from '../hooks/usePokemonPagineted';
+import { FadeInImage } from '../components/FadeInImage';
 
 interface Props extends StackScreenProps<any, any>{}
 
@@ -19,7 +20,13 @@ export const HomeScreen = ({ navigation }: Props) => {
       <FlatList
         data={simplePokemonList}
         keyExtractor={(pokemon) => pokemon.id}
-        renderItem={({ item }) => <Text>{item.name}</Text>}
+        renderItem={({ item }) => (
+          <FadeInImage
+            uri={item.picture}
+            style={{ width: 100, height: 100 }}
+          />
+        )}
+        numColumns={2}
         showsVerticalScrollIndicator={false}
         onEndReached={ loadPokemons }
         onEndReachedThreshold={ 0.4 }
